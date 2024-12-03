accaa748d8bec149f96bbde3c0df503d5fb16263
Dominique Boeckli <dboeckli@gmail.com>
2024-12-03 12:01:58 +0100

    Feature/chapter44 actuator (#7)
    * implement actuator
    
    * use correct port for actuator

--- src/main/java/guru/springframework/spring6reactive/config/SecurityConfig.java
+++ src/main/java/guru/springframework/spring6reactive/config/SecurityConfig.java
@@ -2,23 +2,35 @@ package guru.springframework.spring6reactive.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
+    @Order(1)
+    public SecurityWebFilterChain actuatorSecurityFilterChain(ServerHttpSecurity http) throws Exception {
+        http.securityMatcher(EndpointRequest.toAnyEndpoint())
+            .authorizeExchange(authorize -> authorize.anyExchange().permitAll());
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
     SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        http.csrf(ServerHttpSecurity.CsrfSpec::disable)
-            .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated())
+        http
+            .csrf(ServerHttpSecurity.CsrfSpec::disable)
+            .authorizeExchange
+                (authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated())
             //.pathMatchers("/api/v3/customer/**", "/api/v3/beer/**").hasRole("ADMIN")
             .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()));
         return http.build();
-
     }
 
 }
