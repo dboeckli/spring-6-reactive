111ec15a5969bf68b38d88f1db15fc54f09a1c7c
Dominique Boeckli <dboeckli@gmail.com>
2025-01-22 10:33:47 +0100

    refactor project and build (#14)

--- src/test/java/guru/springframework/spring6reactive/Spring6ReactiveApplicationTests.java
+++ src/test/java/guru/springframework/spring6reactive/Spring6ReactiveApplicationTests.java
@@ -1,17 +1,20 @@
 package guru.springframework.spring6reactive;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @ActiveProfiles(value = "test")
+@Slf4j
 class Spring6ReactiveApplicationTests {
 
     @Test
     void contextLoads() {
+        log.info("Testing Spring 6 Application...");
     }
 
 }
