d5fc38eafc2a1ff5b3ed37dc3875f8a5e39988b2
Dominique Boeckli <dboeckli@gmail.com>
2025-01-28 10:37:11 +0100

    Feature/adapt rest http request (#16)
    * begin
    
    * add openapi interfaces

--- src/main/java/guru/springframework/spring6reactive/config/SecurityConfig.java
+++ src/main/java/guru/springframework/spring6reactive/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring6reactive.config;
 
+import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -7,7 +8,6 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -15,9 +15,9 @@ public class SecurityConfig {
 
     @Bean
     @Order(1)
-    public SecurityWebFilterChain actuatorSecurityFilterChain(ServerHttpSecurity http) throws Exception {
-        http.securityMatcher(EndpointRequest.toAnyEndpoint())
-            .authorizeExchange(authorize -> authorize.anyExchange().permitAll());
+    public SecurityWebFilterChain actuatorSecurityFilterChain(ServerHttpSecurity http) {
+        http.securityMatcher
+                (EndpointRequest.toAnyEndpoint()).authorizeExchange(authorize -> authorize.anyExchange().permitAll());  // allow actuator endpoints to be accessed without authentication
         return http.build();
     }
 
@@ -26,11 +26,17 @@ public class SecurityConfig {
     SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http
             .csrf(ServerHttpSecurity.CsrfSpec::disable)
-            .authorizeExchange
-                (authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated())
-            //.pathMatchers("/api/v3/customer/**", "/api/v3/beer/**").hasRole("ADMIN")
+            .authorizeExchange(authorizeExchange -> authorizeExchange
+                .pathMatchers(
+                    "/v3/api-docs",
+                    "/v3/api-docs.yaml",
+                    "/v3/api-docs/**", 
+                    "/swagger-ui/**", 
+                    "/swagger-ui.html").permitAll() // allow Swagger/Openapi endpoints to be accessed without authentication
+                //.pathMatchers("/api/v3/customer/**", "/api/v3/beer/**").hasRole("ADMIN") // FOR FUTURE USE ONLY
+                .anyExchange().authenticated()
+            )
             .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()));
         return http.build();
     }
-
 }
