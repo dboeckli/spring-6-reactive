b8ce4a30958dc7bcabeac5febb6718026cad9eae
adm-boeckli <dboeckli@gmail.com>
2025-05-21 16:07:57 +0200

    enable deployment 2

--- src/main/java/guru/springframework/spring6reactive/config/LogbookConfig.java
+++ src/main/java/guru/springframework/spring6reactive/config/LogbookConfig.java
@@ -2,7 +2,6 @@ package guru.springframework.spring6reactive.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.zalando.logbook.HttpLogFormatter;
 import org.zalando.logbook.Sink;
 import org.zalando.logbook.json.JsonHttpLogFormatter;
 import org.zalando.logbook.logstash.LogstashLogbackSink;
@@ -12,8 +11,7 @@ public class LogbookConfig {
 
     @Bean
     public Sink logbookLogStash(){
-        HttpLogFormatter formatter = new JsonHttpLogFormatter();
-        return new LogstashLogbackSink(formatter);
+        return new LogstashLogbackSink(new JsonHttpLogFormatter());
     }
     
 }
